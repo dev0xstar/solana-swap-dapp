f6f85f51fc3cec3cc4ca6c217558f88edcb726ef
dev0xstar <angrybird4837@gmail.com>
2024-08-17 18:15:44 -0400

    updated

--- programs/solana-swap-dapp/src/lib.rs
+++ programs/solana-swap-dapp/src/lib.rs
@@ -15,25 +15,8 @@ pub const MAX_STRING_LEN: usize = 50;
 pub mod solana_swap_dapp {
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<Initialize>, 
-        id: String,
-        token_price: Vec<u64>,
-        token_decimal: u8
-    ) -> Result<()> {
-        instructions::initialize::initialize(ctx, id, token_price, token_decimal)
-    }
 
-    pub fn swap(ctx: Context<Swap>, swap_amount: u64) -> Result<()> {
-        instructions::swap::swap(ctx, swap_amount)
-    }
 
-    pub fn withdraw_sol(ctx: Context<WithdrawSol>) -> Result<()> {
-        instructions::withdraw_sol::withdraw_sol(ctx)
-    }
 
-    pub fn withdraw_escrow(ctx: Context<WithdrawEscrow>) -> Result<()> {
-        instructions::withdraw_escrow::withdraw_escrow(ctx)
-    }
 }
 
